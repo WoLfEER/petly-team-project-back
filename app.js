a61785b835cd67d363e9835d84fe3a74daff8e76
WoLfEER <101401666+WoLfEER@users.noreply.github.com>
2022-12-25 14:59:59 +0200

    swagger

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const logger = require("morgan");
 const cors = require("cors");
 
 require("dotenv").config();
-
-
 const dotenv = require("dotenv");
 dotenv.config();
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 //   routes
 const newsRouter = require("./routes/api/news");
@@ -16,9 +16,7 @@ const friendsRouter = require("./routes/api/friends");
 const noticeRouter = require("./routes/api/notices");
 const userRouter = require("./routes/api/user");
 
-
 const app = express();
-
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -26,18 +24,16 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // middleware
 
 app.use("/api/auth", authRouter);
 app.use("/api/news", newsRouter);
 
-
 app.use("/api/friends", friendsRouter);
 app.use("/api/notices", noticeRouter);
 app.use("/api/user", userRouter);
-
-
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
