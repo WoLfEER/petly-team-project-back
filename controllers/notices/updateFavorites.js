71b0b5e725015b9888c307a3e05c79d1d473e148
WoLfEER <101401666+WoLfEER@users.noreply.github.com>
2022-12-30 19:10:50 +0200

    sad

--- controllers/notices/updateFavorites.js
+++ controllers/notices/updateFavorites.js
@@ -6,21 +6,39 @@ const updateFavorites = async (req, res) => {
   const { id: noticeId } = req.params;
 
   const result = await Notice.findById(noticeId);
+  // console.log(result.info);
+
+  // delete
+
+  if (result.info.includes(id)) {
+    const noticeFavoriteDelete = await Notice.findByIdAndUpdate(
+      noticeId,
+      { $pull: { info: id } },
+      { new: true }
+    );
+    const userFavoriteDelete = await User.findByIdAndUpdate(
+      id,
+      { $pull: { favorites: noticeId } },
+      { new: true }
+    );
+
+    res.json({ noticeFavoriteDelete, userFavoriteDelete });
+    return;
+  }
 
   // add
-  const userFavor = await User.findByIdAndUpdate(
+  const noticeFavorite = await Notice.findByIdAndUpdate(
+    noticeId,
+    { $push: { info: id } },
+    { new: true }
+  );
+  const userFavorite = await User.findByIdAndUpdate(
+    id,
     { $push: { favorites: noticeId } },
     { new: true }
   );
 
-  // delete
-  const userFavorDelete = await User.findByIdAndUpdate(
-    id,
-    { $pull: { favorites: noticeId } },
-    { new: true }
-  );
-
-  res.json(result);
+  res.json({ noticeFavorite, userFavorite });
 };
 
 module.exports = updateFavorites;
