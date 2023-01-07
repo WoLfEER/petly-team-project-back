be518a4c09f8dd4638715b695a761adae056dedb
WoLfEER <101401666+WoLfEER@users.noreply.github.com>
2023-01-07 16:34:41 +0200

    fix favs

--- controllers/notices/getOwnNoticeFavorites.js
+++ controllers/notices/getOwnNoticeFavorites.js
@@ -2,8 +2,8 @@ const { User } = require("../../models/user");
 
 const getOwnNoticeFavorites = async (req, res) => {
   const { _id: userId } = req.user;
-  const user = await User.findById(userId);
-  res.json({ code: 200, status: "success", data: user })
+  const { favorites } = await User.findById(userId);
+  res.json({ code: 200, status: "success", data: favorites });
 };
 
 module.exports = getOwnNoticeFavorites;
