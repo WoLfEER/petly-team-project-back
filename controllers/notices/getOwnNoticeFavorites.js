c65d703f9ef8bae9cbfd729a1a876486aa18831e
WoLfEER <101401666+WoLfEER@users.noreply.github.com>
2023-01-07 20:09:41 +0200

    favs

--- controllers/notices/getOwnNoticeFavorites.js
+++ controllers/notices/getOwnNoticeFavorites.js
@@ -2,8 +2,8 @@ const { User } = require("../../models/user");
 
 const getOwnNoticeFavorites = async (req, res) => {
   const { _id: userId } = req.user;
-
-  const {favorites} = await User.find({ _id: userId }).populate("favorites");
+  const user = await User.findById(userId);
+  const { favorites } = await user.populate("favorites");
   res.json({ code: 200, status: "success", data: favorites });
 };
 
